befb837d9fb5f30bb481ccdd16cec950901622ab
Dhyey Buddhadev <dbuddhadev@gmail.com>
2020-04-13 19:39:32 +0530

    Updates

---
+++ precache-manifest.5fb7751d9797dd77631dcf6d7a9d2bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a6f12de4d4a4fdc600536595b945ea",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "ae74fa5062a4309eeb68",
+    "url": "/corona-tracker/static/css/main.9ab7c45f.chunk.css"
+  },
+  {
+    "revision": "ca26dfed733060c481b6",
+    "url": "/corona-tracker/static/js/2.9c86a17c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.9c86a17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae74fa5062a4309eeb68",
+    "url": "/corona-tracker/static/js/main.40883a7b.chunk.js"
+  },
+  {
+    "revision": "7b364b072c19e4e32858",
+    "url": "/corona-tracker/static/js/runtime-main.16ffa1e1.js"
+  }
+]);
